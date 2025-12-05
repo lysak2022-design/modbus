e1b511dbd7028d3a48b704ef2a818d7513869a9e
lysak2022-design <lysak2022@gmail.com>
2025-12-05 20:18:50 +0300

    Delete modbus_proxy directory

--- modbus_proxy/storage/event_storage.py
+++
@@ -1,9 +0,0 @@
-class EventStorage:
-    def __init__(self):
-        self.events = []
-
-    def add_event(self, event: dict):
-        self.events.append(event)
-
-    def get_events(self):
-        return self.events
--- modbus_proxy/storage/log_storage.py
+++
@@ -1,9 +0,0 @@
-class LogStorage:
-    def __init__(self):
-        self.logs = []
-
-    def add_log(self, text: str):
-        self.logs.append(text)
-
-    def get_logs(self):
-        return self.logs
